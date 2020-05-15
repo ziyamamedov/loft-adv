0e274a31346bc2bc541b924becd55c569052ab07
Ziya Mamedov <ziya_90@mail.ru>
2020-05-15 12:14:18 +0300

    Changed public path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/dist/';
+  const publicPath = 'https://ziyamamedov.github.io/loft-adv/dist/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
